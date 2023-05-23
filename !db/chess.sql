86340f99a326a4cc4b745e36380a9d737c88d60f
Luboš Klika <67468578+LubosKlika@users.noreply.github.com>
2023-05-23 18:53:48 +0200

    Initial commit

--- !db/chess.sql
+++ !db/chess.sql
@@ -1,23 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 3.5.8.2
--- http://www.phpmyadmin.net
---
--- Počítač: md372.wedos.net:3306
--- Vygenerováno: Pon 22. kvě 2023, 20:26
--- Verze serveru: 10.3.32-MariaDB-log
--- Verze PHP: 5.4.23
+
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
---
--- Databáze: `d322953_chess`
 --
 
 -- --------------------------------------------------------
