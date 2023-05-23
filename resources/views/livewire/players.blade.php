bb1cdc707a1cf3d6b80d2286a2b1cf34a07820c5
Luboš Klika <67468578+LubosKlika@users.noreply.github.com>
2023-05-23 19:41:44 +0200

    Update players.blade.php

--- resources/views/livewire/players.blade.php
+++ resources/views/livewire/players.blade.php
@@ -54,7 +54,7 @@
     {{ date('d.m.Y H:i', strtotime($joined)) }}</p>
 @endif
 <div class="text-right">
-<button  wire:click="toggleEdit" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">{{ $editable ? 'Cancel' : 'Edit' }}</button>
+    <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" wire:click="{{ $editable ? 'cancel' : 'toggleEdit' }}">{{ $editable ? 'Cancel' : 'Edit' }}</button>
 
 @if($editable)
 
